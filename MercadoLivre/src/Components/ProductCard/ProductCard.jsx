2f6a33d1a15f7576f2ffcbf18cbf1724e79fbd35
RuanDevz <Ruanbatista1509@outlook.com>
2023-12-04 19:45:48 -0300

    comit deploy

--- MercadoLivre/src/Components/ProductCard/ProductCard.jsx
+++ MercadoLivre/src/Components/ProductCard/ProductCard.jsx
@@ -26,11 +26,11 @@ const ProductCard = ({ data }) => {
     <section className='product-card'>
       <img src={modifiedThumbnail} alt='product' className='card-image' />
       <div className='card-info'>
-        <h2 className='card-title'>{title}</h2>
         <h3 className='card-price'>{price.toLocaleString('pt-br', {
           style: 'currency',
           currency: 'BRL',
         })}</h3>
+        <h2 className='card-description'>{title}</h2>
       </div>
       <button  onClick={AddToCart} type='button' className='button-addcart'>
         <FaCartPlus />
